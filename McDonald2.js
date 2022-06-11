e8c95888c8ef9dffe355ad32246c6fe45cb750fb
Siyah <m.siyaudheen@gmail.com>
2022-06-11 18:08:45 +0530

    f

---
+++ McDonald2.js
@@ -0,0 +1,12 @@
+
+let modelBox=document.getElementById("box1")
+
+modelBox.style.display='none'
+
+function openBox() {
+    modelBox.style.display="block"
+}
+
+function cancel(){
+    modelBox.style.display='none'
+}
